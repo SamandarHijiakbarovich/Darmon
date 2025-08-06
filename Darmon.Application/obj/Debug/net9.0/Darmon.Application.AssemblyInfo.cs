8d93dfd6d5ac3275a5f1485b0680bbc0a272a13f
SAMANDAR <dotnetdasturchi@gmail.com>
2025-08-06 17:56:21 +0500

    things

--- Darmon.Application/obj/Debug/net9.0/Darmon.Application.AssemblyInfo.cs
+++ Darmon.Application/obj/Debug/net9.0/Darmon.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Darmon.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1915b5253b506c02daf097344c15d83f7a0db251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ce732e65f0c1015a9eee12c7da1a32a7055bf2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Darmon.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Darmon.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
